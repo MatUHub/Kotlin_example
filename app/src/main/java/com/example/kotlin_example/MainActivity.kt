e3993f95febd87e54555f138a9fbd085560b45e0
MatU <mattves@bk.ru>
2022-02-06 11:29:56 +0300

    подключена Firebase

--- app/src/main/java/com/example/kotlin_example/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_example/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.kotlin_example
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.InputBinding
+import android.widget.EditText
+import com.example.kotlin_example.databinding.ActivityMainBinding
+import java.sql.DatabaseMetaData
 
 class MainActivity : AppCompatActivity() {
+
+   private lateinit var binding: ActivityMainBinding
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
---
+++ app/src/main/java/com/example/kotlin_example/User.kt
@@ -0,0 +1,4 @@
+package com.example.kotlin_example
+
+data class User(val name: String, val secondName: String, val email: String) {
+}
